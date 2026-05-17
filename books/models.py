f269d6b29e0eef1e738f994447e8153f2bb2eeab
baiel <jyrgalbekuulubaiel@gmail.com>
2026-05-17 14:32:53 +0600

    Add main configs, create Book, Author, FavoriteBook models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True)
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    summary = models.TextField(blank=True)
+    isbn = models.CharField(max_length=20, unique=True)
+    authors = models.ManyToManyField(Author, related_name='books')
+    publication_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+
+class FavoriteBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='favorited_by')
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'book')
+        ordering = ['-added_at']
+
+    def __str__(self):
+        return f'{self.user.username} → {self.book.title}'
